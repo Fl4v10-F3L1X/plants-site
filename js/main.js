e5ab771af652c8daf13ace4812e627a039be1988
Fl4v10-F3L1X <flaviofrancisco1802@hotmail.com>
2022-06-08 18:43:28 +0100

    Add About section and they respective style. Add new animation to header section

--- js/main.js
+++ js/main.js
@@ -30,12 +30,13 @@ function linkAction(){
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*================ QUESTIONS ACCORDION ================*/
 /*=========== CHANGE BACKGROUND HEADER============*/
 function scrollHeader(){
     const header = document.getElementById('header')
-    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
+
+/*================ QUESTIONS ACCORDION ================*/
 
 window.addEventListener('scroll', scrollHeader)
